7e0bb41516654837c63e891fe376ba8e15807ccb
Marco Clark <mac325@lehigh.edu>
2023-04-15 12:56:23 -0400

    more customer processing

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.io.*;
 import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
@@ -74,13 +78,60 @@ public class Test {
                     userHNum = Integer.parseInt(kb.nextLine()) - 1;
                 }
 
-                //prompt for dates in format YYYYMMDD
-                System.out.println("Enter check-in date in format YYYYMMDD");
-                String in_date  = kb.nextLine();
-                System.out.println("Enter check-out date in format YYYYMMDD");
-                String out_date = kb.nextLine();
-                
-                //COME BACK AND ADD CHECKS THAT DATES ARE A) FUTURE and b) CHECK OUT IS AFTER CHECK IN
+                //CHECKIN DATE
+                String inDate = "";
+                String outDate = "";
+                Date in = null;
+                Date out = null;
+                System.out.println("Enter check-in date in format MM-dd-yyyy");
+                SimpleDateFormat sdfrmt = new SimpleDateFormat("MM-dd-yyyy");
+                sdfrmt.setLenient(false);
+                while (true){
+                    if (kb.hasNext("[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]")){ //pattern for date
+                        inDate = kb.nextLine();
+                        try {
+                            in = sdfrmt.parse(inDate);
+                            //if we get here, it is a valid date
+                            //want to check if it is before TODAY
+                            if (in.before(new Date())){
+                                System.out.println("Check-in cannot be today or earlier. Enter check-in date in format MM-dd-yyyy");
+                                continue;
+                            }
+                            break;
+                        }
+                        catch (Exception e){
+                            System.out.println("Invalid. Enter check-in date in format MM-dd-yyyy");
+                        }
+                    }
+                    else {
+                        System.out.println("Invalid format. Enter check-in date in format MM-dd-yyyy");
+                        kb.nextLine(); //consume input
+                    }
+                }
+                //CHECKOUT DATE
+                System.out.println("Enter check-out date in format MM-dd-yyyy");
+                while (true){
+                    if (kb.hasNext("[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]")){ //pattern for date
+                        outDate = kb.nextLine();
+                        try {
+                            out = sdfrmt.parse(outDate);
+                            //if we get here, it is a valid date
+                            //want to check if it is before checkin
+                            if (out.before(in) || out.equals(in)){
+                                System.out.println("Check-out cannot be before check-in. Enter check-in date in format MM-dd-yyyy");
+                                continue;
+                            }
+                            break;
+                        }
+                        catch (Exception e){
+                            System.out.println("Invalid. Enter check-out date in format MM-dd-yyyy");
+                        }
+                    }
+                    else {
+                        System.out.println("Invalid format. Enter check-out date in format MM-dd-yyyy");
+                        kb.nextLine(); //consume input
+                    }
+                }
                 
                 //get the most recent reservation id so we can add next num
                 q = "select max(res_id) as mr from reservation";
@@ -91,7 +142,108 @@ public class Test {
                     newResNum = result.getInt("mr") + 1;
                 }
 
+                //display available room types
 
+                q = "select unique rm_type as rt from room where h_id = " + userHNum;
+                result = s.executeQuery(q);
+                ArrayList<String> rmTypes = new ArrayList<>();
+                String userRoom = "";
+                if (!result.next()) System.out.println ("Empty result.");
+                else {  
+                    do {
+                        String curString = result.getString("rt");
+                        rmTypes.add(curString);
+                        System.out.println(curString);
+                    }while (result.next());
+                    
+                    while (!rmTypes.contains(userRoom)){
+                        System.out.println("Enter a room type to reserve:");
+                        userRoom = kb.nextLine();
+                    }
+                    
+                }
+
+                //new or returning customer??
+                System.out.println("If a returning customer, enter your customer id now. Otherwise enter 0:");
+                int cID = -1;
+                int pID = -1;
+                boolean newcus = false;
+                while (true){
+                    if (!kb.hasNextInt()){
+                        System.out.println("Customer id must be a number");
+                        kb.nextLine();
+                    }
+                    else {
+                        cID = kb.nextInt();
+                        if (cID == 0){
+                            //generate new c_ID
+                            q = "select max(cust_id) as mcid from customer";
+                            result = s.executeQuery(q);
+                            result.next();
+                            cID = result.getInt("mcid") + 1;
+                            System.out.println(cID + "\n\n");
+                            newcus = true;
+                        }
+                        break;
+                    }
+                }
+                //collect new customer data
+                kb.nextLine();
+                if (newcus){
+                    System.out.print("Welcome new customer. Enter your name: ");
+                    String newName = kb.nextLine();
+                    System.out.print("Enter your address:");
+                    String newAddr = kb.nextLine();
+                    System.out.println("Enter phone number in form ########## with no dashes or spaces: ");
+                    int pNum = -1;
+                    while (true){
+                        if (!kb.hasNextInt()){
+                            System.out.println("Phone number must consist of numbers only");
+                            kb.nextLine();
+                        }
+                        else {
+                            pNum = kb.nextInt();
+                            //COULD USE MORE VALIDATION
+                            if (pNum < 1000000000){ //i.e not long enough to be valid
+                                System.out.println("Not enough numbers in phone number. Enter phone number in form ########## with no dashes or spaces:");
+                                continue;
+                            }
+                            break; 
+                        }
+                    }
+                    q = "INSERT INTO customer VALUES(" + cID + ",'" + newName + "','" + newAddr + "'," + pNum + ", 2000)";
+                    i = s.executeUpdate(q);
+
+                    //also need a payment
+                    System.out.println("Enter a card number:");
+                    long cnum = -1;
+                    while (true){
+                        if (!kb.hasNextLong()){
+                            System.out.println("Card number must be a number of form ################");
+                            kb.nextLine();
+                        }
+                        else {
+                            cnum = kb.nextLong();
+                            if (cnum < 1000000000000000L){
+                                System.out.println("Invalid. Card number must be a number of form ################");
+                                continue;
+                            }
+                            break;
+                        }
+                    }
+                    
+                }
+
+                
+                //get customer's first payment on file
+                q = "select unique p_id as pd from payment where cust_id = " + cID;
+                
+
+
+
+
+                //now build query to insert
+                // q = "insert into reservation VALUES (" + newResNum + ","  + 
 
                 break;
             case 2:
